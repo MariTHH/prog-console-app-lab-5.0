0b2add43e1bdc47766a3e6e2fbbc57c4da72d6f6
MariTHH <mariia_tolstykh@mail.ru>
2023-04-07 23:31:39 +0300

    Changes by AdminM

--- src/main/java/commands/availableCommands/ExecuteScript.java
+++ src/main/java/commands/availableCommands/ExecuteScript.java
@@ -37,6 +37,7 @@ public class ExecuteScript extends Command {
     @Override
     public void execute(String[] args) throws JAXBException, IOException {
         if (args.length > 2) {
+            //если нет файла ошибка "впишите имя файла"
             System.out.println("Вы неправильно ввели команду");
         } else {
             filePaths.add((String) getArgument());
@@ -47,11 +48,78 @@ public class ExecuteScript extends Command {
                     String line = reader.nextLine().trim();
                     commandList.add(line);
                 }
+
             } catch (FileNotFoundException e) {
                 System.out.println("Файл не найден");
             }
 
-            for (String command : commandList) {
+            for (int i = 0; i < commandList.size(); i++) {
+                while (commandList.get(i).contains("  "))
+                    commandList.get(i).replaceAll("  ", " ");
+
+                String[] commandAndArgument = commandList.get(i).split(" ");
+                String argument;
+
+                if (commandAndArgument.length == 1)
+                    argument = null;
+                else if (commandAndArgument.length == 2)
+                    argument = commandAndArgument[1];
+                else {
+                    System.out.println("Введите комманду и аргумент, если нужно");
+                    return;
+                }
+                Scanner reader = new Scanner(new FileInputStream((String) getArgument()));
+                boolean a = !commandAndArgument[0].equals("add") && !commandAndArgument[0].equals("add_if_min") && !commandAndArgument[0].equals("add_if_max") && !commandAndArgument[0].equals("update");
+                try {
+                    if (commandMap.containsKey(commandAndArgument[0]) && a) {
+                        if (commandAndArgument[0].equals("execute_script")) {
+                            if (filePaths.contains(commandAndArgument[1])) {
+                                System.out.println("Файл содержит рекурсию!!");
+                                continue;
+                            }
+                        }
+                        commandMap.get(commandAndArgument[0]).setArgument(argument);
+                        commandMap.get(commandAndArgument[0]).execute(commandAndArgument);
+
+                    } else if (!a) {
+                        for (int j = 1; j < 11; j++) {
+                            personList.add(commandList.get(i + j));
+                        }
+                        switch (commandAndArgument[0]) {
+                            case "add" -> personCollection.addPerson(ClientManager.createPersonFromScript(personList));
+                            case "add_if_min" -> {
+                                if (personCollection.addIfMinForScript(commandAndArgument[1])) {
+                                    personList.set(6, commandAndArgument[1]);
+                                    personCollection.addPerson(ClientManager.createPersonFromScript(personList));
+                                }
+                            }
+                            case "add_if_max" -> {
+                                if (personCollection.addIfMaxForScript(commandAndArgument[1])) {
+                                    personList.set(6, commandAndArgument[1]);
+                                    personCollection.addPerson(ClientManager.createPersonFromScript(personList));
+                                }
+                            }
+                            case "update" -> {
+                                {
+                                    System.out.println("Введите ID для команды update");
+                                    Update update = new Update(personCollection);
+                                    Scanner scanner = new Scanner(System.in);
+                                    int line = Integer.parseInt(scanner.nextLine().trim());
+                                    if (update.updateForScript(String.valueOf(line))) {
+                                        int id = Integer.parseInt(String.valueOf(line));
+                                        personCollection.updateElement(ClientManager.createPersonFromScript(personList), id);
+                                    }
+                                }
+                            }
+
+                        }
+                        i += 10;
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println("Неверные данные в скрипте, персонаж не создан");
+                }
+            }
+            /*for (String command : commandList) {
                 while (command.contains("  "))
                     command = command.replaceAll("  ", " ");
 
@@ -98,7 +166,7 @@ public class ExecuteScript extends Command {
                     }
                 }
 
-            }
+            }*/
             filePaths.remove(getArgument());
         }
     }
